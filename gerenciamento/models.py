346da9d5b2c728ed3599eb1bfdff40d4b6843857
williamrebeque <williamrebeque@gmail.com>
2021-03-04 18:05:12 -0300

    Correção limite de caracteres do ativo

--- gerenciamento/models.py
+++ gerenciamento/models.py
@@ -9,7 +9,7 @@ class Ativo(models.Model):
 	"""Model for Ativo"""
 	
 	usuario = models.ForeignKey(User, on_delete=models.RESTRICT, null=False)
-	cod_b3 = models.CharField(max_length=5, verbose_name='Código B3')
+	cod_b3 = models.CharField(max_length=6, verbose_name='Código B3')
 	lim_sup = models.FloatField(verbose_name='Vender em')
 	lim_inf = models.FloatField(verbose_name='Comprar em')
 
